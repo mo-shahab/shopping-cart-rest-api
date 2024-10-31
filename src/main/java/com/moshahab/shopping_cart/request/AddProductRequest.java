0ccd52a54707e743ca45f96f15a7bba8c8351db6
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-11-01 00:09:35 +0530

    feat: initial setup and initial rest api structure

---
+++ src/main/java/com/moshahab/shopping_cart/request/AddProductRequest.java
@@ -0,0 +1,19 @@
+package com.moshahab.shopping_cart.request;
+
+import java.math.BigDecimal;
+
+import com.moshahab.shopping_cart.model.Category;
+
+import lombok.Data;
+
+@Data
+public class AddProductRequest {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    private Category category;
+}
