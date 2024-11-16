998698e14417f93bd3982f08971d450434bd10bb
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-11-16 21:13:14 +0530

    feat: user service addition in the service layer

--- src/main/java/com/moshahab/shopping_cart/service/user/UserService.java
+++ src/main/java/com/moshahab/shopping_cart/service/user/UserService.java
@@ -3,6 +3,9 @@ package com.moshahab.shopping_cart.service.user;
 import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.moshahab.shopping_cart.dto.UserDto;
@@ -21,6 +24,7 @@ public class UserService implements IUserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User getUserById(Long id) {
@@ -34,7 +38,7 @@ public class UserService implements IUserService {
                 .map(req -> {
                     User user = new User();
                     user.setEmail(request.getEmail());
-                    user.setPassword(request.getPassword());
+                    user.setPassword(passwordEncoder.encode(request.getPassword()));
                     user.setFirstName(request.getFirstName());
                     user.setLastName(request.getLastName());
                     return userRepository.save(user);
@@ -61,5 +65,12 @@ public class UserService implements IUserService {
     @Override
     public UserDto convertUserToDto(User user) {
         return modelMapper.map(user, UserDto.class);
+    }
+
+    @Override
+    public User getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        return userRepository.findByEmail(email);
     }
 }
